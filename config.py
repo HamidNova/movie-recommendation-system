dcfb2820277d3889930ef1ba48489c81ff485a2d
Zero-Day-Hero <hamidrezamirzaei8363@gmail.com>
2026-05-13 08:29:46 -0400

    Update configuration and dependencies for new hybrid pipeline

--- config.py
+++ config.py
@@ -14,3 +14,26 @@ K_VALUES = [5, 10]
 CROSS_VALIDATION_FOLDS = 5
 
 DEFAULT_CONTENT_WEIGHT = 0.4
+
+POPULAR_MIN_RATINGS = 10
+RANDOM_SEED = 42
+
+# Fuzzy C-Means parameters
+FCM_PARAMS = {
+    'n_clusters': 15,
+    'm': 2.0,
+    'max_iter': 100,
+    'error': 0.005,
+    'random_state': 42
+}
+
+# Hybrid Neural Network parameters (PyTorch)
+HYBRID_NN_PARAMS = {
+    'hidden_units': [128, 64],
+    'learning_rate': 0.001,
+    'epochs': 10,
+    'batch_size': 64
+}
+
+OMDB_API_KEY = "1da7b640"
+DEMO_SAMPLE_SIZE = 500000
